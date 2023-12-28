ee10b4aebeac348068a7f3d66b1807d429d7efd5
lk3v1nn <kevinlmorataya@gmail.com>
2023-12-28 14:01:24 -0600

    variables de entorno

--- src/index.js
+++ src/index.js
@@ -22,6 +22,6 @@ app.get("", (req, res) => {
     res.send("esuchando");
 });
 
-app.listen(3000, () => {
+app.listen(config.port, () => {
     console.log(`escuchando en puerto ${config.port}`);
 });
