7c36f10f1aa10043cc8b9ddd1d0a352f10c6fd8d
lk3v1nn <kevinlmorataya@gmail.com>
2023-12-28 13:25:03 -0600

    conexion con DB establecida

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,30 +1,28 @@
 const sql = require("mssql");
 
-require('dotenv').config()
+require("dotenv").config();
 
 const sqlConfig = {
     user: process.env.DBuser,
     password: process.env.DBpassword,
     database: process.env.DBdatabase,
     server: process.env.DBserver,
-    port: parseInt(process.env.DBPort),
     options: {
         encrypt: true, // for azure
-        trustServerCertificate: true // change to true for local dev / self-signed certs
-      }
+        trustServerCertificate: true, // change to true for local dev / self-signed certs
+    },
 };
 
-async function dbConsultQuery (query) {
+async function dbConsultQuery(query) {
     try {
         await sql.connect(sqlConfig);
-        const dbResponse = await sql.query(query)
-        return dbResponse.recordsets
+        const dbResponse = await sql.query(query);
+        return dbResponse.recordsets;
     } catch (error) {
         console.log("error al conectarse a la base de datos F");
         console.log(error);
-        return 
+        return;
     }
-};
+}
 
-module.exports =  {dbConsultQuery}
-
+module.exports = { dbConsultQuery };
