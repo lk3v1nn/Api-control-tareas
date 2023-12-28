3dcef91fda6043156b048604f7ba00670cca3884
lk3v1nn <kevinlmorataya@gmail.com>
2023-12-28 13:45:26 -0600

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -22,6 +22,6 @@ app.get("", (req, res) => {
     res.send("esuchando");
 });
 
-app.listen(config.port, () => {
+app.listen(3000, () => {
     console.log(`escuchando en puerto ${config.port}`);
 });
